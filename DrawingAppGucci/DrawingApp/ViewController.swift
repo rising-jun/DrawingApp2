5e60e5c39e042767604c359550917ccee8b291f1
Damagucci-Juice <jsr9874@gmail.com>
2022-07-11 14:17:43 +0900

    feat: 사각형 100개 생성후 사각형 버튼 맨 앞에 오도록 설정

--- DrawingAppGucci/DrawingApp/ViewController.swift
+++ DrawingAppGucci/DrawingApp/ViewController.swift
@@ -11,25 +11,30 @@ import os.log
 class ViewController: UIViewController {
     
     private let plane = Plane()
+    @IBOutlet weak var rectangleButton: UIButton!
     @IBOutlet weak var statusView: UIView!
     
     @IBAction func addRectangleTouched(_ sender: Any) {
-        let rectangle = plane.makeRectangle()
-        addRectangleView(rect: rectangle)
+        addRectangleView(rect: plane.makeRectangle())
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         layout()
         attribute()
+        for _ in 0...100 {
+            addRectangleView(rect: plane.makeRectangle())
+        }
     }
     
     private func attribute() {
         statusView.isHidden = true
-    }
+        rectangleButton.isOpaque = true
+        rectangleButton.layer.cornerRadius = 10
+    } 
     
     private func layout() {
-        
+        self.view.bringSubviewToFront(rectangleButton)
     }
     
     private func addRectangleView(rect: Rectangle) {
@@ -43,6 +48,10 @@ class ViewController: UIViewController {
             return view
         }()
         self.view.addSubview(view)
+        self.view.bringSubviewToFront(rectangleButton)
     }
     
 }
+
+
+
