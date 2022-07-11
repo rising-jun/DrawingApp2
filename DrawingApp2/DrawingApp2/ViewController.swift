d1ae5a9fae24599976ecfbdb304696ea93aa0474
kimdongjune <jun36817@naver.com>
2022-07-11 15:01:23 +0900

    [Init]: 프로젝트 초기설정.

---
+++ DrawingApp2/DrawingApp2/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  DrawingApp2
+//
+//  Created by 김동준 on 2022/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
